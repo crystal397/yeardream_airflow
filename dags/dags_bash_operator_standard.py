58d95eeb861a3cd87fb41385cef9ea033d4174ee
crystal <crystal397@cuk.edu>
2024-06-17 09:20:48 +0900

    modified dags_bash_operator_standard.py

--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -9,3 +9,15 @@ my_dag = DAG(
     start_date=datetime.datetime(2024, 6, 1),
     tags=["homework"]
 )
+
+bash_t1 = BashOperator(
+    task_id="bash_t1",
+    bash_command="echo whoami",
+)
+
+bash_t2 = BashOperator(
+    task_id="bash_t2",
+    bash_command="echo $HOSTNAME",
+)
+
+bash_t1 >> bash_t2
