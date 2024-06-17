35ed2ee20f4aa8418fb45b97bcaea1517b840dbb
crystal <crystal397@cuk.edu>
2024-06-17 15:08:37 +0900

    select fruit

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -10,7 +10,7 @@ with DAG(
 ) as dag:
     t0_grant_permission = BashOperator(
         task_id="t0_grant_permission",
-        bash_command="chmod +x /opt/airflow/plugins/shell/select_fruit.sh",
+        bash_command="chmod +x /opt/airflow/plugins/shell/select_fruit.sh ",
     )
     t1_orange = BashOperator(
         task_id="t1_orange",
@@ -22,4 +22,4 @@ with DAG(
         bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
 
-    t0_grant_permission >> [t1_orange >> t2_avocado]
+    t0_grant_permission >> [t1_orange , t2_avocado]
