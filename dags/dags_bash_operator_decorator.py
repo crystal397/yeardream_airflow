91088d7b4ffe418502cc5bf0746253af6fbe1c15
crystal <crystal397@cuk.edu>
2024-06-17 14:58:31 +0900

    decorator change

--- dags/dags_bash_operator_decorator.py
+++ dags/dags_bash_operator_decorator.py
@@ -3,10 +3,10 @@ import datetime
 from airflow.decorators import dag
 from airflow.operators.empty import EmptyOperator
 
-@dag(start_date=datetime.datetime(2024, 5, 1), 
+@dag(dag_id='dags_bash_operator_decorators',
+     start_date=datetime.datetime(2024, 5, 1), 
      schedule="0 13 * * 5#2", 
      tags=["homework"])
-
 def generate_dag():
     EmptyOperator(task_id="dags_bash_operator_standard")
 
