3facbece65fa541778f0b58773756eb4f3e07a65
crystal <crystal397@cuk.edu>
2024-06-17 10:18:22 +0900

    modified dags_bash_operator_standard.py

--- dags/dags_bash_operator_standard.py
+++ dags/dags_bash_operator_standard.py
@@ -1,7 +1,7 @@
 import datetime
 
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 
 my_dag = DAG(
     dag_id="dags_bash_operator_standard",
@@ -10,14 +10,4 @@ my_dag = DAG(
     tags=["homework"]
 )
 
-bash_t1 = BashOperator(
-    task_id="bash_t1",
-    bash_command="echo whoami",
-)
-
-bash_t2 = BashOperator(
-    task_id="bash_t2",
-    bash_command="echo $HOSTNAME",
-)
-
-bash_t1 >> bash_t2
+EmptyOperator(task_id="task", dag=my_dag)
