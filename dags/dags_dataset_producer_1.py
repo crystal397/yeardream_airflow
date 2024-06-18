1ce3795eacf09d7c4a047ead675d443cad5e5b0f
crystal <crystal397@cuk.edu>
2024-06-18 16:48:15 +0900

    producer

--- dags/dags_dataset_producer_1.py
+++ dags/dags_dataset_producer_1.py
@@ -6,12 +6,13 @@ import pendulum
 dataset_dags_dataset_producer_1 = Dataset("dags_dataset_producer_1")
 
 with DAG(
-        dag_id='dags_dataset_consumer_1',
-        schedule=[dataset_dags_dataset_producer_1],
+        dag_id='dags_dataset_producer_1',
+        schedule='0 7 * * *',
         start_date=pendulum.datetime(2024, 6, 16, tz='Asia/Seoul'),
         catchup=False
 ) as dag:
     bash_task = BashOperator(
         task_id='bash_task',
-        bash_command='echo {{ ti.run_id }} && echo "producer_1 이 완료되면 수행"'
+        outlets=[dataset_dags_dataset_producer_1],
+        bash_command='echo "producer_1 수행 완료"'
     )
