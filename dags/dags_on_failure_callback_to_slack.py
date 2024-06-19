34549f3b74b6de2d5c39ea9502ed4b6acfb14ca9
crystal <crystal397@cuk.edu>
2024-06-19 10:44:21 +0900

    slack again

--- dags/dags_on_failure_callback_to_slack.py
+++ dags/dags_on_failure_callback_to_slack.py
@@ -1,35 +1,30 @@
-from airflow.providers.slack.hooks.slack_webhook import SlackWebhookHook
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import timedelta
+import pendulum
+from callbacks.on_failure_callback_to_slack import on_failure_callback_to_slack
 
-def on_failure_callback_to_slack(context):
-    ti = context.get('ti')
-    dag_id = ti.dag_id
-    task_id = ti.task_id
-    err_msg = context.get('exception')
-    batch_date = context.get('data_interval_end').in_timezone('Asia/Seoul')
 
-    slack_hook = SlackWebhookHook(slack_webhook_conn_id='conn_slack_airflow_bot')
-    text = "실패 알람"
-    blocks = [
-        {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": f"*{dag_id}.{task_id} 실패 알람*"
-			}
-		},
-        {
-            "type": "section",
-            "fields": [
-                {
-                    "type": "mrkdwn",
-                    "text": f"*배치 시간*: {batch_date}"
-                },
-                {
-                    "type": "mrkdwn",
-                    "text": f"*에러 내용*: {err_msg}"
-                }
-            ]
-        }
-    ]
+with DAG(
+    dag_id='dags_on_failure_callback_to_slack',
+    start_date=pendulum.datetime(2024, 6, 17, tz='Asia/Seoul'),
+    schedule='0 * * * *',
+    catchup=False,
+    default_args={
+        'on_failure_callback':on_failure_callback_to_slack,
+        'execution_timeout': timedelta(seconds=60)
+    }
 
-    slack_hook.send(text=text, blocks=blocks)
+) as dag:
+    task_slp_90 = BashOperator(
+        task_id='task_slp_90',
+        bash_command='sleep 90',
+    )
+
+    task_ext_1 = BashOperator(
+        trigger_rule='all_done',
+        task_id='task_ext_1',
+        bash_command='exit 1'
+    )
+
+    task_slp_90 >> task_ext_1
